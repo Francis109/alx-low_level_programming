a88f9903654b7a034d772eaad6f67253943ea031
francis109 <francismwape0@gmail.com>
2022-03-14 16:32:54 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: Print numbers 0 - 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
