ca56d7a8397e52ea609d8c09768a64b6144e534a
francis109 <francismwape0@gmail.com>
2022-04-13 15:51:34 -0700

    task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,17 +1,15 @@
 #ifndef CALC_H
 #define CALC_H
 
-/*
- * File: 3-calc.h
- * 
- * Desc: Header file containing all structures and
- *       prototypes used by the 3-main.c program.
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * struct op - A struct op.
- * @op: The operator.
- * @f: The associated function.
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
@@ -19,11 +17,11 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 
-#endif
+#endif /* CALC_H */
