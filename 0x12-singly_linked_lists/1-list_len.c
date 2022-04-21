5543d73f180bf09ef6bfac9f7fa2292c8c8c227b
francis109 <francismwape0@gmail.com>
2022-04-21 14:23:54 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,26 +1,18 @@
-/*
- * File: 1-list_len.c
- * 
- */
-
 #include "lists.h"
-
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
- * @h: The linked list_t list.
- *
- * Return: The number of elements in h.
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the `list_t` list
+ * Return: number of elements in list
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		elements++;
+		n++;
 		h = h->next;
 	}
 
-	return (elements);
+	return (n);
 }
